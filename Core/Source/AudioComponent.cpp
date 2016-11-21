5b63f90a9306419fb79a32070302e25d89e7c765
Izzy Benavente <ibenaven@usc.edu>
2016-11-20 20:27:34 -0800

    Temp solution to pcmreadcallback syntax issues

--- Core/Source/AudioComponent.cpp
+++ Core/Source/AudioComponent.cpp
@@ -1,9 +1,6 @@
 #include "ITPEnginePCH.h"
 
-IMPL_COMPONENT(AudioComponent, Component, 100);
-
 AudioComponent::AudioComponent(Actor& owner)
-	:Component(owner)
 {
 	FMOD::System_Create(&system);
 
@@ -12,14 +9,14 @@ AudioComponent::AudioComponent(Actor& owner)
 	mainChannel.Play( skiesOpen );
 }
 
-FMOD_RESULT F_CALLBACK AudioComponent::WriteSoundData( FMOD_SOUND * sound, void * data, unsigned int length )
+FMOD_RESULT F_CALLBACK WriteSoundData( FMOD_SOUND *sound, void *data, unsigned int datalen )
 {
 	// Cast to PCM and calculate sample count
 	PCM16* pcmData = ( PCM16* ) data;
 	// Number of samples that can fit in array when each sample is 2 bytes
-	int pcmDataCount = length / 2;
+	int pcmDataCount = datalen / 2;
 
-	mainChannel.WriteSoundData( pcmData, pcmDataCount );
+	//mainChannel.WriteSoundData( pcmData, pcmDataCount );
 
 	return FMOD_OK;
 }
@@ -32,22 +29,20 @@ FMOD_RESULT AudioComponent::PlayAudioData()
 	info.cbsize = sizeof( FMOD_CREATESOUNDEXINFO );
 
 	// 44100 Hz, Signed 16 bit format, 2 channels
-	info.defaultfrequency = 44100;
+	info.defaultfrequency = SAMPLE_RATE;
 	info.format = FMOD_SOUND_FORMAT_PCM16;
 	info.numchannels = 2;
 
 	// 5 second duration
 	info.length = 44100 * 2 * sizeof(signed short) * 5;
-	info.decodebuffersize = 4410;
-
-	// Specify the callback function that will provide the audio data
-	info.pcmreadcallback = WriteSoundData;
+	info.decodebuffersize = SAMPLE_RATE * 0.1;	// Number of samples submitted every 100ms
+	info.pcmreadcallback = WriteSoundData; //FMOD_SOUND_PCMREAD_CALLBACK
 
 	// Create a looping stream with FMOD_OPENUSER and the info we filled
 	FMOD::Sound* sound;
 	FMOD_MODE mode = FMOD_LOOP_NORMAL | FMOD_OPENUSER;
 	system->createStream( 0, mode, &info, &sound );
-	system->playSound( sound, NULL, false, 0 ); // 2nd param: Channel group defaults to FMOD_CHANNEL_FREE
+	//system->playSound( sound, NULL, false, 0 ); // 2nd param: Channel group defaults to FMOD_CHANNEL_FREE
 
 	return FMOD_OK;
 }
--- Core/Source/AudioComponent.h
+++ Core/Source/AudioComponent.h
@@ -1,22 +1,22 @@
 #pragma once
-#include "Component.h"
 #include "Channel.h"
 #include <fmod.hpp>
 
-class AudioComponent : public Component
+#define SAMPLE_RATE 44100
+
+class AudioComponent
 {
-	DECL_COMPONENT(AudioComponent, Component);
 public:
 	AudioComponent( Actor& owner );
 	FMOD_RESULT PlayAudioData();
-	FMOD_RESULT F_CALLBACK WriteSoundData( FMOD_SOUND* sound, void* data, unsigned int length );
+	//FMOD_RESULT F_CALLBACK WriteSoundData( FMOD_SOUND *sound, void *data, unsigned int datalen );
 
 private:
 	// Mono: count = sampling rate * duration * channels
-	PCM16 data[44100];
+	PCM16 data[SAMPLE_RATE];
 
 	// Stereo: count = 44100 Hz * 1 second * 2 channels
-	PCM16 stereoData[88200];
+	PCM16 stereoData[SAMPLE_RATE * 2];
 	Channel mainChannel;
 	FMOD::System* system;
 };
